f70068872793ece1f001fce8001377e6cd5fba25
unknown <mahalakshmi.xworkz@gmail.com>
2023-04-23 19:47:59 +0100

    encapsulation

---
+++ Country/src/com/xworkz/country/Runner.java
@@ -0,0 +1,80 @@
+package com.xworkz.country;
+
+import com.xworkz.country.info.Countryinfo;
+
+public class Runner {
+	
+	 public static void main(String[] args) {
+		 Countryinfo info=new Countryinfo();
+		info.setname("India");
+		info.setanimal("tiger");
+		info.setcontinent("Asia");
+		info.setbird("peacock");
+		info.setCapital("New Delhi");
+		info.setfruit("mango");
+		info.setflower("Lotus");
+		info.setnoOfStates(29);
+		info.setpm("Narendhra Modi");
+		info.settree("Baniyan Tree");
+		info.setsymbol("asoka sakra");
+		info.setgate("Mumbai");
+		info.setdivision(4);
+		info.setreptile("Ganges crocodile");
+		
+		info.setrainfall("cherrapunji");
+		info.setdesert("the thar");
+		info.setmangrove("sundarban forests");
+		info.setpopulation(1300000000);
+		info.setliterature(7700000);
+		info.setsanctuary(592);
+		info.setnationalpark(106);
+		info.setforest(23000000);
+		info.setfirstpm("Nehru");
+		info.setfamous("Thanjai Periya Kovil");
+		info.setflag(3);
+		info.setfreedom(1947);
+		info.setheritage("Elephant");
+		info.setrepublic(1950);
+		info.setanthem("Jana Gana Mana");
+		info.setarea(328000000);
+		
+		
+		
+		System.out.println(info.getname());
+		System.out.println(info.getcontinent());
+		System.out.println(info.getanimal());
+		System.out.println(info.getbird());
+		System.out.println(info.getcapital());
+		System.out.println(info.getflower());
+		System.out.println(info.getfruit());
+		System.out.println(info.getpm());
+		System.out.println(info.getstates());
+		System.out.println(info.getsymbol());
+		System.out.println(info.gettree());
+		
+		System.out.println(info.getgate());
+		System.out.println(info.getreptile());
+		System.out.println(info.getdevision());
+		System.out.println(info.getforest());
+		System.out.println(info.getliterature());
+		System.out.println(info.getmangrove());
+		System.out.println(info.getnationalpark());
+		System.out.println(info.getpopulation());
+		System.out.println(info.getrainfall());
+		System.out.println(info.getsanctuary());
+		
+		System.out.println(info.getdesert());
+		System.out.println(info.getfamous());
+		System.out.println(info.getFirstpm());
+		System.out.println(info.getheritage());
+		System.out.println(info.getanthem());
+		System.out.println(info.getarea());
+		System.out.println(info.getflag());
+		System.out.println(info.getindependence());
+		System.out.println(info.getrepublic());
+		 
+		
+	 }
+	
+
+}
---
+++ Country/src/com/xworkz/country/info/Countryinfo.java
@@ -0,0 +1,232 @@
+package com.xworkz.country.info;
+
+public class Countryinfo {
+	private String  name;
+	private String continent ;
+	private String PM;
+	private int noOfStates;
+	private String Capital;
+	private String NationalSymbol;
+	private String NationalAnimal ;
+	private String NationalBird;
+	private String NationalFruit ;
+	private String NationalFlower;
+	private String Nationaltree;
+	private String Nationalreptile;
+	private String NationalGate;
+	private int devisionofcountry;
+	
+	private String largerainfall;
+	private String desert;
+	private String mangrove;
+	private long  population;
+	private long literaturerate;
+	private long forestrange;
+	private int noOfnationalpark;
+	private int WildSanuatary;
+	
+	private String FirstPM;
+	private int independenceyear;
+	private int republicyear;
+	private long area;
+	private int flagcolours;
+	private String heritageanimal;
+	private String Nationalanthem;
+	private String famousplace;
+	
+	
+	
+	
+	public String getname() {
+		return name;
+	}
+	public String getcontinent() {
+		return continent;
+	}
+	public String getpm() {
+		return PM;
+	}
+	public int  getstates() {
+		return noOfStates;
+	}
+	public String getcapital() {
+		return Capital;
+	}
+	public String getsymbol() {
+		return NationalSymbol;
+	}
+	public String getanimal() {
+		return NationalAnimal;
+	}
+	public String getbird() {
+		return NationalBird;
+	}
+	public String getfruit() {
+		return NationalFruit;
+	}
+	public String getflower() {
+		return NationalFlower;
+	}
+	public String gettree() {
+		return Nationaltree;
+	}
+	public String getreptile() {
+		return Nationalreptile;
+	}
+	public String getgate() {
+		return NationalGate;
+	}
+	public int getdevision() {
+		return devisionofcountry;
+	}
+	public String getrainfall() {
+		return largerainfall;
+	}
+	public String getdesert() {
+		return desert;
+	}
+	public String getmangrove() {
+		return mangrove;
+	}
+	public long getpopulation() {
+		return population;
+	}
+	public long getliterature() {
+		return literaturerate;
+	}
+	public long  getforest() {
+		return forestrange;
+	}
+	public int getsanctuary() {
+		return WildSanuatary;
+	}
+	public int getnationalpark() {
+		return noOfnationalpark;
+	}
+	public String getFirstpm() {
+		return  FirstPM;
+	}
+	public  int getindependence() {
+		return  independenceyear;
+	}
+	public  int getrepublic() {
+		return  republicyear;
+	}
+	public long getarea  () {
+		return  area;
+	}
+	public   int  getflag() {
+		return   flagcolours;
+	}
+	public String getheritage() {
+		return heritageanimal;
+	}
+	public String getanthem() {
+		return  Nationalanthem;
+	}
+	public String getfamous() {
+		return   famousplace;
+	}
+	
+	
+	 ///setter
+	
+	
+	public void setname(String name) {
+		this.name=name;
+	}
+	public void setcontinent(String continent) {
+		this.continent= continent;
+	}
+	public void setnoOfStates(int noOfStates) {
+		this.noOfStates=noOfStates;
+	}
+	public void setpm(String PM) {
+		this.PM=PM;
+	}
+	public void setCapital(String Capital) {
+		this.Capital=Capital;
+	}
+	public void setanimal(String NationalAnimal) {
+		this.NationalAnimal=NationalAnimal;
+	}
+	public void setbird(String NationalBird ) {
+		this.NationalBird=NationalBird;
+	}
+	public void setfruit(String NationalFruit ) {
+		this.NationalFruit=NationalFruit;
+	} 
+	public void setflower(String NationalFlower ) {
+		this.NationalFlower=NationalFlower;
+	}
+	public void settree(String Nationaltree) {
+		this.Nationaltree=Nationaltree;
+	}
+	public void setreptile(String Nationalreptile) {
+		this.Nationalreptile=Nationalreptile;
+	}
+	public void setsymbol(String NationalSymbol) {
+		this.NationalSymbol=NationalSymbol;
+	}
+	public void setdivision(int divisionofcountry) {
+		this. devisionofcountry=  devisionofcountry;
+	}
+	public void setgate(String NationalGate) {
+		this.NationalGate=NationalGate;
+	}
+	 
+	public void setrainfall(String largerainfall) {
+		this.largerainfall=largerainfall;
+	}
+	public void setdesert(String  desert) {
+		this.desert=desert;
+	}
+	public void setmangrove(String  mangrove) {
+		this.mangrove=mangrove;
+	}
+	public void setpopulation(long population) {
+		this.population= population;
+	}
+	public void setliterature(long literaturerate ) {
+		this.literaturerate=literaturerate;
+	}
+	public void setforest( long forsetrange) {
+		this.forestrange=forsetrange;
+	}
+	public void setnationalpark( int noOfnationalpark) {
+		this.noOfnationalpark= noOfnationalpark;
+	}
+	public void setsanctuary( int WildSanuatary)   {
+		this. WildSanuatary= WildSanuatary;
+	}
+	 
+	
+	public void setfirstpm(String FirstPM)   {
+		this.FirstPM= FirstPM ;
+	}
+	public void setfreedom (int independenceyear )   {
+		this.independenceyear=  independenceyear ;
+	}
+	public void setrepublic(int republicyear )   {
+		this.republicyear=   republicyear ;
+	}
+	public void setarea(long  area )   {
+		this.area=  area ;
+	}
+	public void setheritage ( String heritageanimal)   {
+		this.heritageanimal= heritageanimal ;
+	}
+	public void setanthem ( String Nationalanthem )   {
+		this.Nationalanthem=  Nationalanthem ;
+	}
+	public void setflag (int  flagcolours )   {
+		this. flagcolours=  flagcolours ;
+	}
+	 
+	public void setfamous ( String famousplace )   {
+		this.famousplace=  famousplace ;
+	}
+	  
+	 
+	 
+}
